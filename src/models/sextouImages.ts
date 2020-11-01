ca48cba3b4e1de785edbbc2a35d19cb6090671dd
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-11-01 15:43:44 -0300

    .

--- src/models/sextouImages.ts
+++ src/models/sextouImages.ts
@@ -1,8 +1,7 @@
-
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm'
 
 @Entity('sextouImages')
-export default class SextouImages {
+export default class SextouImages extends BaseEntity {
   @PrimaryGeneratedColumn('increment')
   id: number
 
