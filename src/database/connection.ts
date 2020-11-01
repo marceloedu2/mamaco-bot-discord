5e1e61350222145f1b6a756babab0822a6d11c6e
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-11-01 17:32:58 -0300

    .

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,19 @@
-import { createConnection } from 'typeorm'
+import { ConnectionOptions, createConnection } from 'typeorm'
 
-createConnection().then()
+const rootDir = process.env.NODE_ENV === 'development' ? './src' : './dist'
+
+const config: ConnectionOptions = {
+  type: 'sqlite',
+  database: './src/database/database.sqlite',
+  migrations: [`./${rootDir}/database/migrations/*.{js,ts}`],
+  entities: [`./${rootDir}/models/*.{js,ts}`],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+}
+
+createConnection(config)
+  .then(() => {
+    console.log('Connected to database')
+  })
+  .catch(error => console.log(error))
