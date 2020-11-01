bfac98b7311d0d707bb4475e7124a2e453f25b5a
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-11-01 13:56:19 -0300

    add comando sextou (add e exibir)

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,7 @@ module.exports = async (client, message, args) => {
   const m = await message.channel.send('ping')
 
   m.edit(
-    `🏓 **| Pong!**\nLatência do Server: **${
+    `🏓 **| Pongsss!**\nLatência do Server: **${
       m.createdTimestamp - message.createdTimestamp
     }ms.**\nLatência da API: **${Math.round(client.ws.ping)}ms**`,
   )
