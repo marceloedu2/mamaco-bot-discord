a306866a380994c132de250ed9e063cb99f5cd0a
Marcelo Araújo <46998976+marceloedu2@users.noreply.github.com>
2021-04-27 13:57:11 -0300

    config db

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,7 @@ const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist'
 
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  url: process.env.DATABASE_URL,
   migrations: [`./${rootDir}/database/migrations/*.{js,ts}`],
   entities: [`./${rootDir}/models/*.{js,ts}`],
   cli: {
