0a57e390d8d318af53f07a0b8b4e9347f3780e6f
Marcelo Araújo <46998976+marceloedu2@users.noreply.github.com>
2021-04-26 22:44:00 -0300

    teste dyno

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 import cors from 'cors'
-import express from 'express'
+import express, { request } from 'express'
 
 import DiscordBot from './client'
 
@@ -15,7 +15,6 @@ app.get('/', (request, response) => {
 })
 
 
-
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server started port:3333')
 })
@@ -23,3 +22,10 @@ app.listen(process.env.PORT || 3333, () => {
 DiscordBot.getInstance().connect()
 
 console.log('Successfully conected');
+
+let reqTimer = setTimeout(function wakeUp() {
+   request("https://mamaco-bot-app.herokuapp.com/", () => {
+      console.log("WAKE UP DYNO");
+   });
+   return reqTimer = setTimeout(wakeUp, 1200000);
+}, 1200000);
