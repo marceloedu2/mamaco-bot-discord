8c24d7203f92e666687a9af8df7929b3cf159555
Marcelo Araújo <marcelo.edu2@gmail.com>
2021-09-20 13:43:47 -0300

    fix: correction bot (#72)
    Co-authored-by: Marcelo Araujo <marceloedu2@gmail.com>

--- src/commands/randomstuff/cat.ts
+++ src/commands/randomstuff/cat.ts
@@ -1,15 +1,26 @@
+import axios from 'axios'
 import Discord from 'discord.js'
 
 import getRandomColors from '../../utils/getRandomColors'
 
-const Cats = async (client, message, args) => {
-  const catGif = await 'http://thecatapi.com/api/images/get'
+const Cats = async (_, message) => {
+  try {
+    const { data } = await axios.get(
+      'https://api.thecatapi.com/v1/images/search',
+    )
 
-  const embed = new Discord.MessageEmbed()
-    .setColor(getRandomColors())
-    .setDescription(`Look i found a cute cat :cat:`)
-    .setImage(catGif)
+    const embed = new Discord.MessageEmbed()
+      .setColor(getRandomColors())
+      .setDescription(`Look i found a cute cat :cat:`)
+      .setImage(data[0].url)
 
-  return message.channel.send(embed)
+    return message.channel.send(embed)
+  } catch (err) {
+    const embed = new Discord.MessageEmbed()
+      .setColor('#FF0000')
+      .setDescription('Unexpected error in command!')
+
+    return message.channel.send(embed)
+  }
 }
 module.exports = Cats
